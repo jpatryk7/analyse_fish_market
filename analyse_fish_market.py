63aecd82db0973aabe1b5a755192f8a3f64209db
jpatryk7 <pjesionka@spartaglobal.com>
2022-09-21 20:21:21 +0100

    AnalyseFishMarket and tests completed

--- analyse_fish_market.py
+++ analyse_fish_market.py
@@ -1,5 +1,7 @@
 import boto3
 import pandas as pd
+import settings
+import uuid
 
 
 class AnalyseFishMarket:
@@ -9,39 +11,70 @@ class AnalyseFishMarket:
         with "fish-". Put all files' content in one dataframe.
         :return:
         """
-        self.fish_market_filepaths = []
-        self.fish_df = pd.DataFrame()
-        pass
+        self.bucket_name = settings.bucket_name
+        self.directory_name = settings.directory_name
+        self.filename_prefix = settings.filename_prefix
+        self.upload_path = settings.upload_path
+
+        self.s3_client = boto3.client('s3')
+
+        self.fish_market_filepaths = self._find_fish_files()
+        self.fish_df = self._read_all_to_df(self.fish_market_filepaths)
 
     def _find_fish_files(self) -> list[str]:
         """
         Find all files starting with the filename_prefix - default "fish-".
         :return: list with all paths to files starting with the prefix
         """
-        return [""]
+        path_list = []
+        for path in self.s3_client.list_objects_v2(Bucket=self.bucket_name, Prefix=self.directory_name)["Contents"]:
+            if path["Key"].split('/')[-1].startswith(self.filename_prefix):
+                path_list.append(path["Key"])
+        return path_list
 
-    def _read_all_to_df(self, filepath_list: list[str]) -> pd.DataFrame:
+    def _read_all_to_df(self, filepath_list: list[str], *, local: bool = False) -> pd.DataFrame:
         """
         Read all .csv given files from the S3 bucket, convert each to dataframe and merge them together vertically.
         :param filepath_list: list with all paths to files starting with the prefix
+        :param local: optional parameter - if set to true, function will search local machine for the files
         :return: dataframe with content of all .csv files at paths from the list
         """
-        return pd.DataFrame()
+        df_list = []
+        for path in filepath_list:
+            obj = pd.read_csv(path if local else self.s3_client.get_object(Bucket=self.bucket_name, Key=path)["Body"])
+            df_list.append(obj)
+        df = pd.concat(df_list)
+        return df
 
-    def avg_rows(self) -> pd.DataFrame:
+    def avg_rows(self, key: str = "Species") -> pd.DataFrame:
         """
         Calculate avg value of each row
         :return: dataframe with avg value of each row of self.fish_df
         """
-        return pd.DataFrame()
+        avg_df = []
+        for species in self.fish_df[key].unique():
+            avg_df.append(self.fish_df[self.fish_df[key] == species].mean(numeric_only=True))
+        return pd.DataFrame(avg_df, index=self.fish_df[key].unique()).transpose()
 
-    def upload_df_as_file(self, df: pd.DataFrame, delete_local: bool = False) -> None:
+    def upload_df_as_file(self, df: pd.DataFrame, *, upload: bool = True) -> str:
         """
         Create a .csv file locally and upload it on cloud. Optionally remove the local .csv file after the operation is
         finished.
         :param df: dataframe to upload to the cloud as csv file
-        :param delete_local: optional flag that specifies if the locally created .csv should be removed after the
-        opeartion is done
+        :param upload: setting upload to False suppresses uploading the file to the cloud
         :return:
         """
-        pass
+        local_filename = "csv_dump/" + str(uuid.uuid4()) + ".csv"
+        with open(local_filename, "w") as csv_file:
+            df.to_csv(local_filename)
+
+        if upload:
+            self.s3_client.upload_file(Filename=local_filename, Bucket=self.bucket_name, Key=self.upload_path)
+
+        return local_filename
+
+
+if __name__ == "__main__":
+    fish_market = AnalyseFishMarket()
+    fish_market_avg = fish_market.avg_rows()
+    fish_market.upload_df_as_file(fish_market_avg)
--- test_analyse_fish_market.py
+++ test_analyse_fish_market.py
@@ -1,84 +1,82 @@
 import unittest
+import os
 from analyse_fish_market import *
 
 
 class TestAnalyseFishMarket(unittest.TestCase):
     def setUp(self) -> None:
-        pass
+        # settings variables
+        self.bucket_name = settings.bucket_name
+        self.directory_name = settings.directory_name
+        self.filename_prefix = settings.filename_prefix
+        self.upload_path = settings.upload_path
 
-    def test_fish_market_filepaths_start_with(self) -> None:
+        # boto3
+        self.s3_resource = boto3.resource('s3')
+        self.s3_client = boto3.client('s3')
+        self.files_at_the_dir = [o["Key"] for o in self.s3_client.list_objects_v2(
+            Bucket=self.bucket_name,
+            Prefix=self.directory_name
+        )["Contents"]]
+
+        # AnalyseFishMarket class' object
+        self.fish = AnalyseFishMarket()
+        self.avg_data_df = self.fish.avg_rows()
+
+    def test_fish_market_filepaths_path_format(self) -> None:
+        """
+        Test if each entry is a string of format "path/to/the-file-etc.csv"
+        :return:
+        """
+        expected = {
+            "slash_count": self.directory_name.count("/") + 1,
+            "dash_count": self.filename_prefix.count("-"),
+            "file_extension": ".csv"
+        }
+        for actual in self.fish.fish_market_filepaths:
+            self.assertIsInstance(actual, str)
+            self.assertEqual(expected["slash_count"], actual.count("/"))
+            self.assertTrue(expected["dash_count"] <= actual.split("/")[-1].count("-"))
+            self.assertIn(expected["file_extension"], actual.split("/")[-1])
+
+    def test_fish_market_filepaths_starts_with(self) -> None:
         """
         Test if all filepaths start with settings.filename_prefix (default: "fish-")
         :return:
         """
-        pass
+        expected = self.filename_prefix
+        for actual in [path.split('/')[-1] for path in self.fish.fish_market_filepaths]:
+            self.assertIn(expected, actual)
 
-    def test_fish_market_filepaths_count(self) -> None:
+    def test_fish_market_filepaths_duplicates(self) -> None:
         """
-        Test if all filepaths start with settings.filename_prefix were found
+        Test if there are any duplicates in the fish_market_filepaths list
         :return:
         """
-        pass
+        expected = 0
+        actual = len(self.fish.fish_market_filepaths) - len(set(self.fish.fish_market_filepaths))
+        self.assertEqual(expected, actual)
 
-    def test_fish_df_col_count(self) -> None:
+    def test_fish_market_filepaths_match(self) -> None:
         """
-        Test if all columns were written to the dataframe
+        Test if paths in fish_market_filepaths are also in self.files_at_the_dir
         :return:
         """
-        pass
+        expected = self.files_at_the_dir
+        for actual in self.fish.fish_market_filepaths:
+            self.assertIn(actual, expected)
 
-    def test_fish_df_col_names(self) -> None:
-        """
-        Check for correct column names
-        :return:
-        """
-        pass
-
-    def test_fish_df_row_count(self) -> None:
-        """
-        Test if all rows were written to the dataframe
-        :return:
-        """
-        pass
-
-    def test_avg_rows_col_count(self) -> None:
-        """
-        Test if all columns were written to the dataframe
-        :return:
-        """
-        pass
-
-    def test_avg_rows_col_names(self) -> None:
-        """
-        Check for correct column names
-        :return:
-        """
-        pass
-
-    def test_avg_rows_row_count(self) -> None:
-        """
-        Test if all rows were written to the dataframe
-        :return:
-        """
-        pass
-
-    def test_upload_df_as_file_on_cloud(self) -> None:
-        """
-        Check if the uploaded file exists on cloud in correct directory
-        :return:
-        """
-        pass
-
-    def test_upload_df_as_file_delete_local_true(self) -> None:
-        """
-        Check if the file was deleted in the project directory
-        :return:
-        """
-        pass
-
-    def test_upload_df_as_file_delete_local_false(self) -> None:
+    def test_upload_df_as_file_local(self) -> None:
         """
         Check if the file exists locally
         :return:
         """
-        pass
+        local_filename = self.fish.upload_df_as_file(
+            self.avg_data_df,
+            upload=False
+        )
+        self.assertTrue(os.path.exists(local_filename))
+
+
+if __name__ == "__main__":
+    unittest.main()
